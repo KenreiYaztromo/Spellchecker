98da7af4d7d159969c3ef794f34650069503561b
KenreiYaztromo <raphly@protonmail.com>
2019-11-18 01:56:37 +0000

    new function

--- spellcheck.py
+++ spellcheck.py
@@ -20,3 +20,14 @@ words = load_words('spell.words')
 # now check if the word zygotic is a word
 print(check_word(words, 'zygotic'))
 
+def check_words(words, sentence):
+    words_to_check = sentence.split(' ')
+    for word in words_to_check:
+        if not check_word(words, word):
+            print('Word is misspelt : ' + word)
+            return False
+    return True
+
+words = load_words('spell.words')
+print(check_word(words, 'zygotic'))
+print(check_words(words, 'zygotic mistasdas elementary'))
