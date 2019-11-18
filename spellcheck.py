8daf2ce7d4adbeb6025435a48f8f5e175d8f0b15
KenreiYaztromo <raphly@protonmail.com>
2019-11-18 01:41:16 +0000

    first input

--- spellcheck.py
+++ spellcheck.py
@@ -9,3 +9,14 @@ words = open('spell.words').readlines()
 words = list(map(lambda x: x.strip(), words))
 print('zygotic' in words)
 
+def load_words(file_name):
+    words = open(file_name).readlines()
+    return list(map(lambda x: x.strip(), words))
+
+def check_word(words, word):
+    return word in words
+
+words = load_words('spell.words')
+# now check if the word zygotic is a word
+print(check_word(words, 'zygotic'))
+
